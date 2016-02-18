3ccada82c1c6ced80cf7724ade5fc25886738fff
Sindre Sorhus <sindresorhus@gmail.com>
2016-02-18 17:59:43 +0700

    update for latest AVA

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 const reVersion = /\d+\.\d+\.\d+/;
 
 test('latest version', async t => {
-	t.regexTest(reVersion, await fn('ava'));
+	t.regex(await m('ava'), reVersion);
 });
 
 test('latest version scoped', async t => {
-	t.regexTest(reVersion, await fn('@sindresorhus/df'));
+	t.regex(await m('@sindresorhus/df'), reVersion);
 });
