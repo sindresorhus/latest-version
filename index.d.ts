53df0b9c71a2c35edc9a6874ced5a0ab5af116b7
Guo Yunhe <i@guoyunhe.me>
2024-02-04 15:29:42 +0800

    feat: add registryUrl to options

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,10 @@ export type Options = {
 	A semver range or [dist-tag](https://docs.npmjs.com/cli/dist-tag).
 	*/
 	readonly version?: string;
+	/**
+	The registry URL is by default inferred from the npm defaults and `.npmrc`. This is beneficial as `package-json` and any project using it will work just like npm. This option is*only** intended for internal tools. You should __not__ use this option in reusable packages. Prefer just using `.npmrc` whenever possible.
+	*/
+	readonly registryUrl?: string;
 };
 
 /**
